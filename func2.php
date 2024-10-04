57a15cd8c2cbce597905250437b04adc8cb67c09
NasirUddinNasir <cs.nasiruddin@gmail.com>
2024-10-04 09:00:49 +0500

    2nd commit

--- func2.php
+++ func2.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","myhmsdb");
+$con=mysqli_connect("mysql","nasir","hello@123","myhmsdb");
 if(isset($_POST['patsub1'])){
 	$fname=$_POST['fname'];
   $lname=$_POST['lname'];
